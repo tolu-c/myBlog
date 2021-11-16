dca78271d0d0dcf7470b621435a4a40b24b828a1
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-11-16 20:06:29 +0100

    created react app and configured tailwind css

---
+++ my-blog/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container bg-blue-600">
+      <p className="capitalize font-bold text-white pl-4">my blog</p>
+    </div>
+  );
+}
+
+export default App;
